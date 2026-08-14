36d8574802b316d4f256bae7e60f34f54f0cf400
Shubham V. <shubhamd.vispute@gmail.com>
2026-08-15 00:04:37 +0530

    prime Number

--- Git_Learning.py
+++ Git_Learning.py
@@ -1,1 +1,17 @@
-print("Hey! How are you !!")
+class prime_number:
+    def is_Prime(self,number):
+        if number <=1:
+            return False
+        for i in range(2,int(number ** 0.5) +1):
+            if number % i ==0:
+                return False
+        return True
+        
+num = int(input("Enter the Number: "))
+obj = prime_number()
+
+if obj.is_Prime(num):
+    print("Prime Number")
+else:
+    print("Not Prime")
+
