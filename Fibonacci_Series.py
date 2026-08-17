bf2da8eea000852a6a5d36dedf3dda50c9ad6893
Shubham V. <shubhamd.vispute@gmail.com>
2026-08-17 23:21:11 +0530

    Fibonacci added

--- Fibonacci_Series.py
+++ Fibonacci_Series.py
@@ -1,10 +1,10 @@
-class Fibonacci:
+class fibonacci:
     @staticmethod
     def fibo(n):
-        a,b = 0,1
+        a , b = 0 , 1
         series = []
         for _ in range(n):
             series.append(a)
             a , b = b, a+b
         return series
-    print(fibo(10))
+print(fibonacci.fibo(10))
